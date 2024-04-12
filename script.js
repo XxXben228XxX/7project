299c53aeb978449bab37ad5b5bfa45cdcdbb7882
XxXben228XxX <83588259+XxXben228XxX@users.noreply.github.com>
2024-04-12 15:42:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ function calculate() {
     let result = eval(document.querySelector('.display').value);
     document.querySelector('.display').value = result;
 }
+
+function toggleSign() {
+    let displayValue = document.querySelector('.display').value;
+    if (displayValue !== '' && displayValue !== '0') {
+        document.querySelector('.display').value = displayValue.charAt(0) === '-' ? displayValue.slice(1) : '-' + displayValue;
+    }
+}
+
+function addDecimal() {
+    let displayValue = document.querySelector('.display').value;
+    if (displayValue.indexOf('.') === -1) {
+        document.querySelector('.display').value += '.';
+    }
+}
