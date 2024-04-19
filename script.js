dd63ba6b09fa6ad145325e6cc6b0c645a6a3223a
XxXben228XxX <83588259+XxXben228XxX@users.noreply.github.com>
2024-04-19 16:07:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function appendToDisplay(value) {
     let displayValue = document.querySelector('.display').value;
-    if (displayValue === '0' && value !== 'x^2' && value !== 'sqrt(') {
+    if (displayValue === '0') {
         document.querySelector('.display').value = value;
     } else {
         document.querySelector('.display').value += value;
